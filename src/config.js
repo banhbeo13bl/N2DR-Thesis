8bc9e35df93d94d03197240ea0889310f5cd35b2
banhbeo13bl <ncmtriet@gmail.com>
2023-05-25 13:16:39 +0700

    Commint 1

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+export const NFTCONTRACT = "0xf8adafedb9225a4d236477ca66b71a816ec188a2";
+export const STAKINGCONTRACT = "0x6de1b2e2416da8b94ce1a53395368e085f86ffe9";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/N8ZEJN2XSB36JA45XG1FU6KYNBYH2TUJHW";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmavM8Zpo9bD3r4zEnhbbBLLvHyfr1YL7f1faG3ovaeSSG/";
+export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/8AX5AP2TU6G45ctsOXNt8K4Fz_BkhhZG");
