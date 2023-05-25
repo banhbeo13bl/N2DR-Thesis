3e2a4212cc0596af8e3b7c5d9dd737415413882c
banhbeo13bl <ncmtriet@gmail.com>
2023-05-25 23:37:45 +0700

    Commit 2

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 import { createAlchemyWeb3 } from '@alch/alchemy-web3';
-export const NFTCONTRACT = "0xf8adafedb9225a4d236477ca66b71a816ec188a2";
-export const STAKINGCONTRACT = "0x6de1b2e2416da8b94ce1a53395368e085f86ffe9";
-export const polygonscanapi = "https://api-testnet.polygonscan.com/N8ZEJN2XSB36JA45XG1FU6KYNBYH2TUJHW";
+export const TOKENCONTRACT = "0x53ee7b08141556f6bf779B5899f1d4AB61086287";
+export const NFTCONTRACT = "0x6803Ce87d37DAb7874f3dbb0294C6ada19BC7762";
+export const STAKINGCONTRACT = "0x96269600711aA767f384543E66DB024CeA620052";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
 export const moralisapi = "https://deep-index.moralis.io/api/v2/";
 export const nftpng = "https://ipfs.io/ipfs/QmavM8Zpo9bD3r4zEnhbbBLLvHyfr1YL7f1faG3ovaeSSG/";
 export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/8AX5AP2TU6G45ctsOXNt8K4Fz_BkhhZG");
+//10000000000000000000000//
